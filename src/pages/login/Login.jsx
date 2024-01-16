e8691804aa0b475a712ce77d9c6583ff334a2160
w3modabbir <modabbirhossen.web@gmail.com>
2024-01-16 22:34:06 +0600

    login_registrationdone

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,44 +9,96 @@ import AuthNavigate from '../../componants/AuthNavigate';
 import './login.scss'
 import Images from '../../utilities/Images';
 import LoginImages from "../../assets/images/login_img.jpg"
-
+import { HiMiniEyeSlash } from "react-icons/hi2";
+import { useState } from 'react';
+import { LiaEyeSolid } from "react-icons/lia";
+import { Modal} from '@mui/material';
+import { RxCross1 } from "react-icons/rx";
 
 const Login = () => {
+  let [showPass,  setShowPass] = useState(false)
+
+  let handleShowPass = () =>{
+    setShowPass((prevShowPass) => !prevShowPass);
+  }
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleModleClose = () => {
+    setOpen(false);
+  }
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 500,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 5,
+  };
   return (
-    <Box>
-    <Grid container spacing={0}>
-      <Grid item xs={6}>
-        <div className="login_txt_main">
-          <div className='login_item'>
-              <SectionHeading style="auth_heading" text="Login to your account!"/>
-              <div className='provider_login'>
-                <img src={GoogleImg}/>
-                <span>Login with Google</span>
+    <>
+      <Box>
+        <Grid container spacing={0}>
+          <Grid item xs={6}>
+            <div className="login_txt_main">
+              <div className='login_item'>
+                  <SectionHeading style="auth_heading" text="Login to your account!"/>
+                  <div className='provider_login'>
+                    <img src={GoogleImg}/>
+                    <span>Login with Google</span>
+                  </div>
+                  <div className='from_main'>
+                    <div>
+                      <Input name="email" type="email" variant="standard" lebelTxt="Email Adress" style="login_input_filed" placeholder="Your Email"/>
+                    </div>
+                    <div>
+                      <Input name="password" type={showPass ? "text" : "password"} variant="standard" lebelTxt="Password" style="login_input_filed" placeholder="Enter your password"/>
+                      <button className='pass_btn' onClick={handleShowPass}>{showPass ? <LiaEyeSolid /> : <HiMiniEyeSlash />}
+                      </button>
+                    </div>
+                    <CustomBtn styling="submitBtn" variant="Contained" text="Login to Continue"/>
+                  </div>
+                  <div>
+                    <AuthNavigate style="sign_txt" link="/registration" linktext="Sign up" text="Don’t have an account ?"/>
+                    <p onClick={handleOpen} className='forgat_pass'>
+                      Forgotten password?
+                    </p>
+                  </div>
               </div>
-              <div className='from_main'>
-                <div>
-                  <Input name="email" type="email" variant="standard" lebelTxt="Email Adress" style="login_input_filed" placeholder="Your Email"/>
-                </div>
-                <div>
-                  <Input name="password" type="password" variant="standard" lebelTxt="Password" style="login_input_filed" placeholder="Enter your password"/>
-                </div>
-                <CustomBtn styling="submitBtn" variant="Contained" text="Login to Continue"/>
-              </div>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className="login_imgbox">
               <div>
-                <AuthNavigate style="sign_txt" link="/registration" linktext="Sign up" text="Don’t have an account ?"/>
+                <Images style="login_img" source={LoginImages} alt="Image Not Found"/>
               </div>
-          </div>
-        </div>
-      </Grid>
-      <Grid item xs={6}>
-        <div className="login_imgbox">
-          <div>
-            <Images style="login_img" source={LoginImages} alt="Image Not Found"/>
-          </div>
-        </div>
-      </Grid>
-    </Grid>
-    </Box>
+            </div>
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className='forgat_pass'>
+              <button className='forgat_iconbtn' onClick={handleModleClose}><RxCross1 /></button>
+              <h2>Find Your Account</h2>
+              <Input type="email" variant="standard" lebelTxt="Email Adress"/>
+              <CustomBtn styling="forgatbtn"   text="Send Link" variant="Contained"/>
+            </div>
+          </Box>
+      </Modal>
+
+    </>
   )
 }
 
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import "./registration.scss"
 import SectionHeading from '../../componants/SectionHeading';
@@ -10,25 +9,35 @@ import AuthNavigate from '../../componants/AuthNavigate';
 import Images from '../../utilities/Images';
 import RegisterImg from "../../assets/images/register_img.jpg"
 import PeraGrap from '../../componants/PeraGrap';
+import { HiMiniEyeSlash } from "react-icons/hi2";
+import { LiaEyeSolid } from "react-icons/lia";
 
 const Registration = () => {
+  let [showPass,  setShowPass] = useState(false)
+
+  let handleShowPass = () =>{
+    setShowPass((prevShowPass) => !prevShowPass);
+  }
   return (
     <Box>
       <Grid container spacing={0}>
         <Grid item xs={6}>
-          <div className="login_txt_main">
-            <div className='login_item'>
+          <div className="register_txt_main">
+            <div className='register_item'>
                 <SectionHeading style="auth_heading" text="Get started with easily register"/>
                 <PeraGrap style="peragrap" text="Free register and you can enjoy it"/>
                 <div className='from_main'>
                   <div>
-                    <Input name="email" type="email" variant="outlined" lebelTxt="Email Adress" style="login_input_filed" placeholder="Your Email"/>
+                    <Input name="email" type="email" variant="outlined" lebelTxt="Email Adress" style="register_input_filed" placeholder="Your Email"/>
                   </div>
                   <div>
-                    <Input name="email" type="email" variant="outlined" lebelTxt="Full Name" style="login_input_filed" placeholder="Enter Your Name"/>
+                    <Input name="email" type="email" variant="outlined" lebelTxt="Full Name" style="register_input_filed" placeholder="Enter Your Name"/>
                   </div>
                   <div>
-                    <Input name="password" type="password" variant="outlined" lebelTxt="Password" style="login_input_filed" placeholder="Enter your password"/>
+                    <Input name="password" type={showPass ? "text" : "password"} variant="outlined" lebelTxt="Password" style="register_input_filed" placeholder="Enter your password"/>
+
+                    <button className='pass_btn' onClick={handleShowPass}>{showPass ? <LiaEyeSolid /> : <HiMiniEyeSlash />}
+                    </button>
                   </div>
                   <CustomBtn styling="submitBtn" variant="Contained" text="Sign up"/>
                 </div>
@@ -39,9 +48,9 @@ const Registration = () => {
           </div>
         </Grid>
         <Grid item xs={6}>
-          <div className="login_imgbox">
+          <div className="register_imgbox">
             <div>
-              <Images style="login_img" source={RegisterImg} alt="Image Not Found"/>
+              <Images style="register_img" source={RegisterImg} alt="Image Not Found"/>
             </div>
           </div>
         </Grid>
