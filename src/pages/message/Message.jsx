f99aaeebe4d5cb773033fe1521b40e08df9d9245
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-03-26 09:54:37 +0600

    massege part sart

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './message.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
+import Images from '../../utilities/Images';
 
 
 
@@ -26,42 +27,43 @@ const Message = () => {
       });
   
     },[])
+
+
+
   return (
     <div className='message_main'>
       <div className='message_user_body'>
         <h3 className='list_heading'>Friend List</h3>
         <div className="sms_user_main">
-          {friendList && friendList.lenght > 0
+          {friendList && friendList.length > 0
           ?
            friendList.map((item, index)=>(
-            // <div key={index} className="sms_user_item"></div>
-            <div key={index} className="user_item">
-          <div className="user_img_box">
-            {data.uid == item.whoSendid
-              ?
-              <Images source={item.whoReceivephoto} alt="img not found"/>
-              :
-              <Images source={item.whoSendphoto} alt="img not found"/>
+            <div key={index} className="sms_user_item">
+              <div className="user_img_box">
+                {data.uid == item.whoSendid
+                  ?
+                  <Images source={item.whoReceivephoto} alt="img not found"/>
+                  :
+                  <Images source={item.whoSendphoto} alt="img not found"/>
 
-            }
-          </div>
-          <div className='friend_info_main'>
-          <div className='user_name'>
-            {data.uid == item.whoSendid
-              ?
-              <h3>{item.whoReceivename}</h3>
-              :
-              <h3>{item.whoSendname}</h3>
+                }
+              </div>
+              <div className='friend_info_main'>
+              <div className='user_name'>
+                {data.uid == item.whoSendid
+                  ?
+                  <h3>{item.whoReceivename}</h3>
+                  :
+                  <h3>{item.whoSendname}</h3>
 
-            }
-            <p>MERN Developer</p>
-          </div>
-            <button className='button'>
-              block
-            </button>
-          </div>
-        </div>
-
+                }
+                <p>MERN Developer</p>
+              </div>
+                <button className='button' onClick={()=>handlefriendblock(item)}>
+                  Message
+                </button>
+              </div>
+            </div>
           ))
           :
           <h2 className='no_friend_props'>No Friend</h2>
